c8cb75404d14569d1f36a2ff5c9868d0422b528b
Mark Pitt <markjpitt@gmail.com>
2013-04-22 17:27:29 +0100

    Added Prism & Unity via NuGet

--- HustleMusic/App.xaml.cs
+++ HustleMusic/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,16 @@ namespace HustleMusic
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            IUnityContainer container = new UnityContainer();
+            container.RegisterInstance("bbfb")
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
     }
 }
